95d9de222086450d0f05e4cf91102a5f9b37055a
gozenburgh <42454219+gozenburgh@users.noreply.github.com>
2018-08-16 13:43:25 -1000

    first commit
    the first commit

---
+++ arraypush.js
@@ -0,0 +1,8 @@
+var studentName = [];
+var lastName = [];
+var ta = prompt("firstname!");
+var ki = prompt("lastname");
+studentName.unshift(ta);
+lastName.unshift(ki);
+window.alert(studentName[0] + lastName[0] + studentName.legnth + lastName.legnth);
+//credit to Eric
